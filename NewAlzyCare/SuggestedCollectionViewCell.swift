26adc96ef938f538df24fcceeb0524fb5bd33286
Rishita <rishitakumari206@gmail.com>
2024-05-22 16:23:56 +0530

    HomeScreen Added

---
+++ NewAlzyCare/SuggestedCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SuggestedCollectionViewCell.swift
+//  NewAlzyCare
+//
+//  Created by Gaurav on 20/05/24.
+//
+
+import UIKit
+
+class SuggestedCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var suggestedImageView: UIImageView!
+    @IBOutlet var suggestedTextLabel: UILabel!
+}
--- NewAlzyCare/ViewController.swift
+++ NewAlzyCare/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        SuggestedDataModel.suggestedGames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = suggestedCollectionView.dequeueReusableCell(withReuseIdentifier: "SuggestedCell", for: indexPath) as? SuggestedCollectionViewCell
+        
+        cell?.suggestedImageView.image = UIImage(named: SuggestedDataModel.suggestedGames[indexPath.row].gameImage)
+        cell?.suggestedTextLabel.text = SuggestedDataModel.suggestedGames[indexPath.row].gameName
+        
+        return cell!
+    }
+    
+//   collectionViews Outlets
+    @IBOutlet var suggestedCollectionView: UICollectionView!
+    
+    
+//    ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let suggestedNib = UINib(nibName: "Suggested", bundle: nil)
+        suggestedCollectionView.register(suggestedNib, forCellWithReuseIdentifier: "SuggestedCell")
+        
+        suggestedCollectionView.dataSource = self
+        suggestedCollectionView.setCollectionViewLayout(generateLayout(), animated: true)
+        
     }
+    
+    func generateLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
 
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .groupPagingCentered
+            section.interGroupSpacing = 0 // No space between groups
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0) // No space from left and right
 
+            return UICollectionViewCompositionalLayout(section: section)
+        }
 }
 
